d266908c6bc443a66fabcd1caf26e4edfafb966a
PopovA <alex.popov@inlinegroup-c.ru>
2022-11-30 15:42:23 +0300

    Версия 1

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        float sideA = readTriangleSide("A", scanner);
+        float sideB = readTriangleSide("B", scanner);
+        float sideC = readTriangleSide("C", scanner);
+
+        Triangle triangle = new Triangle(sideA, sideB, sideC);
+
+        printTriangleHeight("A", triangle.getHeightA());
+        printTriangleHeight("B", triangle.getHeightB());
+        printTriangleHeight("C", triangle.getHeightC());
+    }
+
+    /**
+     * Ввод стороны треугольника
+     */
+    private static float readTriangleSide(String sideName, Scanner scanner)
+    {
+        System.out.printf("Введите длину стороны %s треугольника: ", sideName);
+        float side = scanner.nextFloat();
+
+        if (side <= 0) throw new IllegalArgumentException("Длина должна быть больше нуля");
+
+        return side;
+    }
+
+    /**
+     * Вывод высоты треугольника
+     */
+    private static void printTriangleHeight(String sideName, float height)
+    {
+        System.out.printf("Высота стороны %s: %.3f\n", sideName, height);
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Triangle {
+    float sideA;
+    float sideB;
+    float sideC;
+
+    public Triangle(float sideA, float sideB, float sideC)
+    {
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    private float getHeight(float side)
+    {
+        // Сначала назвал semiperimeter, потом переименовал в p
+        float p = (sideA + sideB + sideC) / 2;
+        return 2 / side * (float) Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+    }
+
+    public float getHeightA()
+    {
+        return getHeight(sideA);
+    }
+
+    public float getHeightB()
+    {
+        return getHeight(sideB);
+    }
+
+    public float getHeightC()
+    {
+        return getHeight(sideC);
+    }
+}
